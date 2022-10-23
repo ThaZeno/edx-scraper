8fc3617b9fff78dd34e56f14f9fec892399143bd
ThaZeno <95727259+ThaZeno@users.noreply.github.com>
2022-10-23 10:50:47 -0400

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,15 +1,4 @@
 const fetch = require("fetch")
 let query = `INSERT_QUERY_HERE`
 
-let lowerCaseQ = query.toLowerCase()
-
-let finalQ = lowerCaseQ.split(' ').join('+')
-
-let toScrap = `https://edx.org/search?q=algebra`
-
-// Array for HTML data 
-var links = []
-fetch.fetchUrl(toScrap, function(error, meta, body){ 
-    console.log(body.toString())
-    
-})
+// WIP
